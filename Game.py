21671b3f4644d05ea0b3f69244bb76adb8e9a2f8
MNTSheridan <146022584+MNTSheridan@users.noreply.github.com>
2023-10-03 18:31:48 +0000

    Beta Version 1.0.0

--- Game.py
+++ Game.py
@@ -1,31 +1,7 @@
-#link to GIT repository: https://github.com/MNTSheridan/PROGassignment1
-'''
-git config user.name "MNTSheridan"
-git config user.email "tran172@sheridancollege.ca"
-'''
-menuMessage = '''
-It is the year 1872, you and your band of cowboys are the wild west's most notorious ciminal organization in the midwest, 
-you and your pals plan for a train robbery... Choose your role!
-\nA. Hijacker, you are responsible with hijacking doors (Unlocking doors are faster with this class)
-\nB. Demoman, you are responsible for opening vaults (Opening container vaults are faster with this class)
-\nC. Robber, you are a responsible for collecting money (Money rewards are higher with this class)
-\nD. Gunman, you are responsible for combating security and cops (Physical power is higher with this class)
-\n\nChoice: '''
-
-classChoiceDict = {
-    "A":"Hijacker",
-    "B":"Demoman",
-    "C":"Robber",
-    "D":"Gunman"
-}
-
-def menu():
-    menuOption = input(menuMessage)
-    if menuOption == None:
-        print("You better pick an option!")
-        menu()
-    else:
-        characterClass = classChoiceDict[menuOption]
-        print(f"You chose {characterClass}! ")
-
-menu()
+Class playerInfo:
+    def __init__(self):
+        self.dxt = 0
+        self.str = 0
+        self.int = 0
+        self.hlt = 0
+    
